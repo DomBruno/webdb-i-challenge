e322e74c154293a9a01a0408b4db0fa5da543212
Dominick Bruno <dbruno@unseen.is>
2019-11-12 23:30:30 -0500

    SQL Queries completed; knex queries written but router has error

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
+// Import Modules
 const express = require('express');
+const helmet = require('helmet');
 
+// Import Data Source
 const db = require('./data/dbConfig.js');
 
+// Create Server Object
 const server = express();
 
+// Import Router
+const acccountsRouter = require('./routers/accountsRouter');
+
+
+// Register Global Middleware
+server.use(helmet());
 server.use(express.json());
+
+// Bind to Root URL
+server.use('/api/accounts', accountsRouter);
+
+// Call handler for root "/"
+server.get('/', (req, res) => {
+    res
+    .status(418)
+    .send("I'm a teapot!");
+});
 
 module.exports = server;
